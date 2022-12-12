e95885ecf09017668a353611a650940f867b72e0
Joni Gg <jonisouthanva@gmail.com>
2022-12-12 11:15:44 +0100

    Add existing project files to Git

---
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView taskRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskRecyclerView = findViewById(R.id.tasksRecyclerView);
+    }
+}
